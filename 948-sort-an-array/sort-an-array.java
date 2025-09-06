aba107876cfab53631d9e8ad73ae2875b8245215
Yashaswee Acharya <129647172+acharya-git@users.noreply.github.com>
2025-09-06 10:59:31 +0530

    Time: 18 ms (89.78%) | Memory: 56 MB (59.60%) - LeetSync

--- 948-sort-an-array/sort-an-array.java
+++ 948-sort-an-array/sort-an-array.java
@@ -4,3 +4,19 @@ class Solution {
         return nums;
     }
 }
+// for(int i=0;i<nums.length;i++){
+//             boolean swapped = false;
+//             for(int j=1;j<nums.length-i;j++){
+//                 if(nums[j]<nums[j-1]){
+//                     int temp = nums[j];
+//                     nums[j] = nums[j-1];
+//                     nums[j-1] = temp;
+//                     swapped = true; 
+//                 }
+//             }
+//             if(!swapped){
+//                 break;
+//             }
+//         }
+//         return nums;
+//     }
